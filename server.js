351c338f2e65042bcbdd87328ad9a31acf78704b
Danfeng Chen <cdf@Danfengs-MacBook-Pro.local>
2018-05-01 17:14:46 -0700

    update from master

--- server.js
+++ server.js
@@ -23,13 +23,17 @@ app.use(express.static("client/build"))
 app.get('/api/allUsers', controller.allUsers)
 // create user
 app.post('/api/newUser', controller.newUser)
-// find one user
+// find user data
 app.get('/api/user/:id', controller.findUser)
-// buy stock
-app.post('/api/user/:id/buy', controller.buyStock)
+// get portfolio
+app.get('/api/user/:id/portfolio', controller.getPortfolio)
+// get history
+app.get('/api/user/:id/history', controller.getHistory)
+// buy and sell stock
+app.post('/api/user/:id/trade', controller.stockTrade)
 
 // app.use(routes)
-// app.get('*', function(req,res) {res.sendFile(path.resolve(__dirname,'../client/build/index.html'))})
+app.get('*', function(req,res) {res.sendFile(path.resolve(__dirname,'../client/build/index.html'))})
 
 const server = app.listen(PORT, () => {
 	console.log("App listening on PORT: " + PORT)
