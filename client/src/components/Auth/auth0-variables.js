d763a55104bdf9f0ec54ed266939c5441c2772c0
Nema Darban <nema.darban@gmail.com>
2018-05-01 18:33:05 -0700

    Adding profile lookup function
    Will need to connect this to redux

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,5 +1,8 @@
 export const AUTH_CONFIG = {
   domain: 'devious.auth0.com',
   clientId: 'CVSJgRVxWoZ46VIAWvU9Jz29ZzhA9vEA',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback',
+  audience: 'https://devious.auth0.com/userinfo',
+  responseType: 'code token id_token',
+  scope: 'openid email profile'
 }
