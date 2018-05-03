5172cc29bfaa42147f712b700126535111debd3c
Nema Darban <nema.darban@gmail.com>
2018-05-02 21:02:47 -0700

    moved auth0 profile consumption to navbar

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -1,41 +1,13 @@
 import React, { Component } from "react";
 import BarChart from "../BarChart/BarChart.js"
 import PieChart from "../PieChart/PieChart.js"
-import * as userActionCreators from "../../actions/userActions"
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+
 import SuggestedStock from "../SuggestedStock/SuggestedStock.js"
 import MyStocks from "../MyStocks/MyStocks.js"
 
-function mapStateToProps(state) {
-  return {
-    user: state.user.email,
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    userActions: bindActionCreators(userActionCreators, dispatch),
-    
-  };
-}
 
 class Portfolio extends Component {
-    componentWillMount() {
-        this.setState({ profile: {} });
-        const { userProfile, getProfile } = this.props.auth;
-        console.log(this.props.auth)
-
-        if (!userProfile) {
-          getProfile((err, profile) => {
-            this.setState({ profile });
-            console.log(profile['email'])
-            this.props.userActions.getUser(profile['email'])
-          });
-        } else {
-          this.setState({ profile: userProfile });
-        }
-    }
-
+    
     render() {
         return(
         <div>
@@ -55,4 +27,4 @@ class Portfolio extends Component {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Portfolio)
+export default Portfolio
