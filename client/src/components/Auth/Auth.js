143052ed0378593365ab4272507f8a3c8654f4bd
Nema Darban <nema.darban@gmail.com>
2018-05-08 19:43:01 -0700

    fixed saving tokens

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -68,7 +68,7 @@ export default class Auth {
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     // navigate to the home route
-    history.replace('/');
+    window.location.reload()
   }
 
   isAuthenticated = () => {
