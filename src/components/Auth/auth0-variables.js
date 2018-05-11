c5a453371c41233b2e9a6628c829da159b85e8d0
Nema Darban <nema.darban@gmail.com>
2018-05-11 07:46:10 -0700

    dynamically setting callback url

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'devious.auth0.com',
   clientId: 'CVSJgRVxWoZ46VIAWvU9Jz29ZzhA9vEA',
-  callbackUrl: 'https://bears-and-bulls.herokuapp.com/callback',
+  callbackUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/callback' : 'https://bears-and-bulls.herokuapp.com/callback',
   audience: 'https://devious.auth0.com/userinfo',
   responseType: 'code token id_token',
   scope: 'openid email profile'
